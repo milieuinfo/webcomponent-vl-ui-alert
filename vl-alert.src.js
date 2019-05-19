b9d526d8e4f2203b3d1256636fa73c83968cb3b2
Tom Coemans <tom.coemans@gmail.com>
2019-05-19 17:49:49 +0200

    WEBC-87 fix

--- vl-alert.src.js
+++ vl-alert.src.js
@@ -1,4 +1,4 @@
-import { NativeVlElement } from '/node_modules/vl-ui-core/vl-core.js';
+import { VlElement } from '/node_modules/vl-ui-core/vl-core.js';
 import '/node_modules/vl-ui-icon/vl-icon.js';
 
 /**
