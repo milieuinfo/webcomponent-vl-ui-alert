c11269aeec76ce2e5af99f31556b9dca0beeea3c
zhaodifeng <zhaodi.feng@vlaanderen.be>
2019-08-09 09:35:31 +0200

    UIG-150

--- vl-alert.src.js
+++ vl-alert.src.js
@@ -93,7 +93,7 @@ export class VlAlert extends VlElement(HTMLElement) {
     _getIconTemplate(newValue) {
         return this._template(`
             <div class="vl-alert__icon">
-                <span is="vl-icon" icon="${newValue}"></span>
+                <span is="vl-icon" data-vl-icon="${newValue}"></span>
             </div>
         `);
     };
