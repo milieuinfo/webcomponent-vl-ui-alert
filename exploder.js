fcf6c5a5dc88e26b207f6cae2a2a519481643fa3
cambiph <incubusattax@gmail.com>
2020-01-20 15:45:20 +0100

    Refactor + parameteriseer naam van src.js

--- exploder.js
+++ exploder.js
@@ -1,20 +1,19 @@
 let fs = require('fs');
 let es = require('event-stream');
-
+let config = require('./package.json');
 
 let lineNr = 0;
 
 let output = fs.createWriteStream('output.js', { flags: 'a', encoding: 'utf-8'});
-let stream = fs.createReadStream('vl-alert.src.js')
+let stream = fs.createReadStream(getFilename())
     .pipe(es.split())
     .pipe(es.mapSync((line) => {
         stream.pause();
-        
         lineNr += 1;
         
         if(line.indexOf('@import "') >= 0) {
             console.log('Found import on line ' + lineNr + ": " + line.trim());
-            let filePath = line.split("\"")[1].replace('../', '')
+            let filePath = sanitizeFilepath(line);
             console.log('Reading file from ' + filePath);
             let style = fs.readFileSync(filePath);
             output.write(style + '\r\n');
@@ -28,7 +27,15 @@ let stream = fs.createReadStream('vl-alert.src.js')
         console.log('Something went wrong.', err);
     })
     .on('end', () => {
-        console.log('All done!');
+        console.log('Exploded style successfully!');
         output.end();
     })
 );
+
+function sanitizeFilepath(input) {
+    return input.split("\"")[1].replace('../', '')
+}
+
+function getFilename() {
+    return config.name.replace('-ui', '') + '.src.js'
+}
